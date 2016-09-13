631e4cc6130daa305ffc574b3289cc87755c02ea
Martin Hoffmann <hn@nvnc.de>
2016-09-13 11:33:46 +0200

    Add lookup_records.

--- src/resolv/lookup/mod.rs
+++ src/resolv/lookup/mod.rs
@@ -2,7 +2,9 @@
 
 pub use self::addr::{lookup_addr, LookupAddr, LookupAddrIter};
 pub use self::host::{lookup_host, LookupHost};
+pub use self::records::lookup_records;
 
 mod addr;
 mod host;
+mod records;
 mod search;
---
+++ src/resolv/lookup/records.rs
@@ -0,0 +1,16 @@
+//! Looking up raw records.
+
+use futures::{BoxFuture, Future};
+use ::bits::{DNameSlice, MessageBuf};
+use ::iana::{RRType, Class};
+use super::super::error::Error;
+use super::super::resolver::Resolver;
+use super::search::search;
+
+pub fn lookup_records<N>(resolv: Resolver, name: N, rtype: RRType,
+                         class: Class) -> BoxFuture<MessageBuf, Error>
+                      where N: AsRef<DNameSlice> {
+    search(resolv, name, move |resolv, name| resolv.query(&name, rtype, class))
+        .boxed()
+}
+
