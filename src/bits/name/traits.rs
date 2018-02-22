386ccd8d7e77259f75a58148b827ed3708e86a61
Martin Hoffmann <hn@nvnc.de>
2018-02-22 10:11:05 +0100

    Document the name traits.

--- src/bits/name/traits.rs
+++ src/bits/name/traits.rs
@@ -1,4 +1,6 @@
 //! Domain name-related traits.
+//!
+/// This is a private module. Its public traits are re-exported by the parent.
 
 use std::cmp;
 use bytes::BytesMut;
@@ -16,6 +18,9 @@ use super::relative::RelativeDname;
 /// [`ToRelativeDname`]. It is separate since it has to be generic over the
 /// lifetime of the label reference but we don’t want to have this lifetime
 /// parameter pollute those traits.
+///
+/// [`ToDname`]: trait.ToDname.html
+/// [`ToRelativeDname`]: trait ToRelativeDname.html
 pub trait ToLabelIter<'a> {
     /// The type of the iterator over the labels.
     ///
@@ -66,76 +71,6 @@ impl<'a, 'b, N: ToLabelIter<'b>> ToLabelIter<'b> for &'a N {
 }
 
 
-//------------ ToRelativeDname -----------------------------------------------
-
-/// A type that represents a relative domain name.
-///
-/// In order to be a relative domain name, a type needs to be able to
-/// provide a sequence of labels via an iterator where the last label is not
-/// the root label. The type also needs to be able to compose the wire-format
-/// representation of the domain name it represents which must not be longer
-/// than 255 characters.
-///
-/// The most important types implementing this trait are [`RelativeDname`]
-/// and [`Chain<L,R>`] where `R` is a `ToRelativeDname` itself.
-///
-/// [`Chain<L, R>`]: struct.Chain.html
-/// [`RelativeDname`]: struct.RelativeDname.html
-pub trait ToRelativeDname: Compose + for<'a> ToLabelIter<'a> {
-    /// Creates an uncompressed value of the domain name.
-    ///
-    /// The method has a default implementation that composes the name into
-    /// a new buffer and returns this buffer. If the implementing type can
-    /// create a `RelativeDname` more efficiently, then it should provide its
-    /// own implementation.
-    fn to_name(&self) -> RelativeDname {
-        let mut bytes = BytesMut::with_capacity(self.compose_len());
-        self.compose(&mut bytes);
-        unsafe {
-            RelativeDname::from_bytes_unchecked(bytes.freeze())
-        }
-    }
-
-    /// Returns a byte slice of the content if possible.
-    ///
-    /// This method can is used to optimize comparision operations between
-    /// two values that are indeed flat names.
-    fn as_flat_slice(&self) -> Option<&[u8]> {
-        None
-    }
-
-    fn name_eq<N: ToRelativeDname>(&self, other: &N) -> bool {
-        if let (Some(left), Some(right)) = (self.as_flat_slice(),
-                                            other.as_flat_slice()) {
-            left.eq_ignore_ascii_case(right)
-        }
-        else {
-            self.iter_labels().eq(other.iter_labels())
-        }
-    }
-
-    fn name_cmp<N: ToRelativeDname>(&self, other: &N) -> cmp::Ordering {
-        let mut self_iter = self.iter_labels();
-        let mut other_iter = other.iter_labels();
-        loop {
-            match (self_iter.next_back(), other_iter.next_back()) {
-                (Some(left), Some(right)) => {
-                    match left.cmp(right) {
-                        cmp::Ordering::Equal => {}
-                        res => return res
-                    }
-                }
-                (None, Some(_)) => return cmp::Ordering::Less,
-                (Some(_), None) => return cmp::Ordering::Greater,
-                (None, None) => return cmp::Ordering::Equal
-            }
-        }
-    }
-}
-
-impl<'a, N: ToRelativeDname + 'a> ToRelativeDname for &'a N { }
-
-
 //------------ ToDname -------------------------------------------------------
 
 /// A type that represents an absolute domain name.
@@ -169,12 +104,23 @@ pub trait ToDname: Compose + Compress + for<'a> ToLabelIter<'a> {
 
     /// Returns a byte slice of the content if possible.
     ///
-    /// This method can is used to optimize comparision operations between
+    /// If a value stores the domain name as one single byte sequence, it
+    /// should return a reference to this sequence here. If the name is
+    /// composed from multiple such sequences, it should return `None`.
+    ///
+    /// This method is used to optimize comparision operations between
     /// two values that are indeed flat names.
     fn as_flat_slice(&self) -> Option<&[u8]> {
         None
     }
 
+    /// Tests whether `self` and `other` are equal.
+    ///
+    /// This method can be used to implement `PartialEq` on types implementing
+    /// `ToDname` since a blanket implementation for all pairs of `ToDname`
+    /// is currently impossible.
+    ///
+    /// Domain names are compared ignoring ASCII case.
     fn name_eq<N: ToDname>(&self, other: &N) -> bool {
         if let (Some(left), Some(right)) = (self.as_flat_slice(),
                                             other.as_flat_slice()) {
@@ -215,4 +161,97 @@ pub trait ToDname: Compose + Compress + for<'a> ToLabelIter<'a> {
 }
 
 impl<'a, N: ToDname + 'a> ToDname for &'a N { }
+
+
+//------------ ToRelativeDname -----------------------------------------------
+
+/// A type that represents a relative domain name.
+///
+/// In order to be a relative domain name, a type needs to be able to
+/// provide a sequence of labels via an iterator where the last label is not
+/// the root label. The type also needs to be able to compose the wire-format
+/// representation of the domain name it represents which must not be longer
+/// than 254 characters. This limit has been chosen so that by attaching the
+/// one character long root label, a valid absolute name can be constructed
+/// from the relative name.
+///
+/// The most important types implementing this trait are [`RelativeDname`]
+/// and [`Chain<L,R>`] where `R` is a `ToRelativeDname` itself.
+///
+/// [`Chain<L, R>`]: struct.Chain.html
+/// [`RelativeDname`]: struct.RelativeDname.html
+pub trait ToRelativeDname: Compose + for<'a> ToLabelIter<'a> {
+    /// Creates an uncompressed value of the domain name.
+    ///
+    /// The method has a default implementation that composes the name into
+    /// a new buffer and returns this buffer. If the implementing type can
+    /// create a `RelativeDname` more efficiently, then it should provide its
+    /// own implementation.
+    fn to_name(&self) -> RelativeDname {
+        let mut bytes = BytesMut::with_capacity(self.compose_len());
+        self.compose(&mut bytes);
+        unsafe {
+            RelativeDname::from_bytes_unchecked(bytes.freeze())
+        }
+    }
+
+    /// Returns a byte slice of the content if possible.
+    ///
+    /// This method can is used to optimize comparision operations between
+    /// two values that are indeed flat names.
+    fn as_flat_slice(&self) -> Option<&[u8]> {
+        None
+    }
+
+    /// Tests whether `self` and `other` are equal.
+    ///
+    /// This method can be used to implement `PartialEq` on types implementing
+    /// `ToDname` since a blanket implementation for all pairs of `ToDname`
+    /// is currently impossible.
+    ///
+    /// Domain names are compared ignoring ASCII case.
+    fn name_eq<N: ToRelativeDname>(&self, other: &N) -> bool {
+        if let (Some(left), Some(right)) = (self.as_flat_slice(),
+                                            other.as_flat_slice()) {
+            left.eq_ignore_ascii_case(right)
+        }
+        else {
+            self.iter_labels().eq(other.iter_labels())
+        }
+    }
+
+    /// Returns the ordering between `self` and `other`.
+    ///
+    /// This method can be used to implement both `PartialOrd` and `Ord` on
+    /// types implementing `ToDname` since a blanket implementation for all
+    /// pairs of `ToDname`s is currently not possible.
+    ///
+    /// Domain name order is determined according to the ‘canonical DNS
+    /// name order’ as defined in [section 6.1 of RFC 4034][RFC4034-6.1].
+    /// This section describes how absolute domain names are ordered only.
+    /// We will order relative domain names according to these rules as if
+    /// they had the same origin, i.e., as if they were relative to the
+    /// same name.
+    ///
+    /// [RFC4034-6.1]: https://tools.ietf.org/html/rfc4034#section-6.1
+    fn name_cmp<N: ToRelativeDname>(&self, other: &N) -> cmp::Ordering {
+        let mut self_iter = self.iter_labels();
+        let mut other_iter = other.iter_labels();
+        loop {
+            match (self_iter.next_back(), other_iter.next_back()) {
+                (Some(left), Some(right)) => {
+                    match left.cmp(right) {
+                        cmp::Ordering::Equal => {}
+                        res => return res
+                    }
+                }
+                (None, Some(_)) => return cmp::Ordering::Less,
+                (Some(_), None) => return cmp::Ordering::Greater,
+                (None, None) => return cmp::Ordering::Equal
+            }
+        }
+    }
+}
+
+impl<'a, N: ToRelativeDname + 'a> ToRelativeDname for &'a N { }
 
