dfcad84de2a3c10f303f68f53dcda85d6f8a0110
Martin Hoffmann <hn@nvnc.de>
2018-08-21 19:13:30 +0200

    Update documentation in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,18 @@
-//! A DNS library for Rust.
+//! A DNS library for Rust – Core.
 //!
-//! This crate provides a wide range of modules related to the Domain Name
-//! System. Currently, these are:
+//! This crate provides a number of modules related to the core functionality 
+//! of the Domain Name System. Currently, these are:
 //!
 //! * fundamental types, traits, and implementations for dealing with DNS
 //!   data through the modules [bits] and [iana],
 //! * parsing of master file data (aka zonefiles) in [master],
 //! * data and master file access for various resource record types in
-//!   [rdata],
-//! * an asynchronous stub resolver implementation for querying the DNS
-//!   in [resolv].
+//!   [rdata].
 //!
 //! [bits]: bits/index.html
 //! [iana]: iana/index.html
 //! [master]: master/index.html
 //! [rdata]: rdata/index.html
-//! [resolv]: resolv/index.html
 #![allow(unknown_lints)] // hide clippy-related #allows on stable. 
 
 extern crate bytes;
