c8b8876abed55069cd2a66f4c69670c8cf6b0572
David Bürgin <dbuergin@gluet.ch>
2022-06-18 18:12:09 +0200

    Move resolv::stub module doc to top of file

--- src/resolv/stub/mod.rs
+++ src/resolv/stub/mod.rs
@@ -1,3 +1,14 @@
+//! A stub resolver.
+//!
+//! The most simple resolver possible simply relays all messages to one of a
+//! set of pre-configured resolvers that will do the actual work. This is
+//! equivalent to what the resolver part of the C library does. This module
+//! provides such a stub resolver that emulates this C resolver as closely
+//! as possible, in particular in the way it is being configured.
+//!
+//! The main type is [`StubResolver`] that implements the [`Resolver`] trait
+//! and thus can be used with the various lookup functions.
+
 use self::conf::{
     ResolvConf, ResolvOptions, SearchSuffix, ServerConf, Transport,
 };
@@ -22,16 +33,6 @@ use std::pin::Pin;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::vec::Vec;
-/// A stub resolver.
-///
-/// The most simple resolver possible simply relays all messages to one of a
-/// set of pre-configured resolvers that will do the actual work. This is
-/// equivalent to what the resolver part of the C library does. This module
-/// provides such a stub resolver that emulates this C resolver as closely
-/// as possible, in particular in the way it is being configured.
-///
-/// The main type is [`StubResolver`] that implements the [`Resolver`] trait
-/// and thus can be used with the various lookup functions.
 use std::{io, ops};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpStream, UdpSocket};
