b952c5fd0b60dc387fce28e35fb925c5f7fca505
TCY16 <tom@nlnetlabs.nl>
2022-08-26 10:46:24 +0200

    add formatting

--- examples/client.rs
+++ examples/client.rs
@@ -1,20 +1,19 @@
+use std::net::UdpSocket;
 use std::str::FromStr;
-use std::net::{UdpSocket};
 
 use domain::base::{
-        Dname, MessageBuilder, Rtype, StaticCompressor, StreamTarget,
-        message::Message, opt::AllOptData
+    message::Message, opt::AllOptData, Dname, MessageBuilder, Rtype,
+    StaticCompressor, StreamTarget,
 };
 use domain::rdata::AllRecordData;
 
 fn create_message() -> StreamTarget<Vec<u8>> {
     // Create a message builder wrapping a compressor wrapping a stream
     // target.
-    let mut msg = MessageBuilder::from_target(
-        StaticCompressor::new(
-                StreamTarget::new_vec()
-        )
-    ).unwrap();
+    let mut msg = MessageBuilder::from_target(StaticCompressor::new(
+        StreamTarget::new_vec(),
+    ))
+    .unwrap();
 
     // Set the RD bit and a random ID in the header and proceed to
     // the question section.
@@ -23,16 +22,21 @@ fn create_message() -> StreamTarget<Vec<u8>> {
     let mut msg = msg.question();
 
     // Add a hard-coded question and proceed to the answer section.
-    msg.push((Dname::<Vec<u8>>::from_str("example.com.").unwrap(), Rtype::A)).unwrap();
+    msg.push((
+        Dname::<Vec<u8>>::from_str("example.com.").unwrap(),
+        Rtype::A,
+    ))
+    .unwrap();
 
     // Skip to the additional section
     let mut msg = msg.additional();
 
     // Add an OPT record.
     msg.opt(|opt| {
-            opt.set_udp_payload_size(4096);
-            Ok(())
-    }).unwrap();
+        opt.set_udp_payload_size(4096);
+        Ok(())
+    })
+    .unwrap();
 
     // Convert the builder into the actual message.
     msg.finish().into_target()
@@ -40,7 +44,8 @@ fn create_message() -> StreamTarget<Vec<u8>> {
 
 fn main() {
     // Bind a UDP socket to a kernel-provided port
-    let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
+    let socket =
+        UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
 
     let message = create_message();
 
@@ -48,7 +53,9 @@ fn main() {
     let port: u16 = 53;
 
     // Send message off to the server using our socket
-    socket.send_to(&message.as_dgram_slice(), (server, port)).unwrap();
+    socket
+        .send_to(&message.as_dgram_slice(), (server, port))
+        .unwrap();
 
     // Create recv buffer
     let mut buffer = vec![0; 1232];
@@ -56,22 +63,28 @@ fn main() {
     // Recv in buffer
     socket.recv_from(&mut buffer).unwrap();
 
-    // Parse and print the response 
-    let response = match Message::from_octets(buffer){
+    // Parse and print the response
+    let response = match Message::from_octets(buffer) {
         Ok(response) => Some(response),
         Err(_) => None,
-    }.unwrap();
+    }
+    .unwrap();
 
     for question in response.question() {
         println!("{}", question.unwrap());
     }
 
     // Unpack and parse with all known record types
-    for record in response.answer().unwrap().limit_to::<AllRecordData<_, _>>() {
+    for record in response.answer().unwrap().limit_to::<AllRecordData<_, _>>()
+    {
         println!("{}", record.unwrap());
     }
 
-    for record in response.authority().unwrap().limit_to::<AllRecordData<_, _>>() {
+    for record in response
+        .authority()
+        .unwrap()
+        .limit_to::<AllRecordData<_, _>>()
+    {
         println!("{}", record.unwrap());
     }
 
@@ -80,9 +93,10 @@ fn main() {
         let opt = option.unwrap();
         match opt {
             AllOptData::Nsid(nsid) => println!("{}", nsid),
-            AllOptData::ExtendedError(extendederror) => println!("{}", extendederror),
+            AllOptData::ExtendedError(extendederror) => {
+                println!("{}", extendederror)
+            }
             _ => println!("NO OPT!"),
         }
     }
 }
-
