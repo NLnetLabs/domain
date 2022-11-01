3dc4c19d60f32f8701ae3dc7be9eee0ce52bc5f0
TCY16 <tom@nlnetlabs.nl>
2022-11-01 16:36:45 +0100

    change edns expire to be in line with other edns options

--- src/base/opt/rfc7314.rs
+++ src/base/opt/rfc7314.rs
@@ -1,5 +1,6 @@
 //! EDNS Options from RFC 7314
 
+use core::fmt;
 use super::super::iana::OptionCode;
 use super::super::message_builder::OptBuilder;
 use super::super::octets::{
@@ -10,22 +11,23 @@ use super::CodeOptData;
 
 //------------ Expire --------------------------------------------------------
 
+// @TODO does this change influences more things below?
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Expire(Option<u32>);
+pub struct Expire(u32);
 
 impl Expire {
-    pub fn new(expire: Option<u32>) -> Self {
+    pub fn new(expire: u32) -> Self {
         Expire(expire)
     }
 
     pub fn push<Target: OctetsBuilder + AsRef<[u8]> + AsMut<[u8]>>(
         builder: &mut OptBuilder<Target>,
-        expire: Option<u32>
+        expire: u32
     ) -> Result<(), ShortBuf> {
         builder.push(&Self::new(expire))
     }
 
-    pub fn expire(self) -> Option<u32> {
+    pub fn expire(self) -> u32 {
         self.0
     }
 }
@@ -35,21 +37,11 @@ impl Expire {
 
 impl<Ref: AsRef<[u8]>> Parse<Ref> for Expire {
     fn parse(parser: &mut Parser<Ref>) -> Result<Self, ParseError> {
-        if parser.remaining() == 0 {
-            Ok(Expire::new(None))
-        }
-        else {
-            u32::parse(parser).map(|res| Expire::new(Some(res)))
-        }
+        u32::parse(parser).map(Self::new)
     }
 
     fn skip(parser: &mut Parser<Ref>) -> Result<(), ParseError> {
-        if parser.remaining() == 0 {
-            Ok(())
-        }
-        else {
-            parser.advance(4)
-        }
+        u32::skip(parser)
     }
 }
 
@@ -58,10 +50,7 @@ impl Compose for Expire {
         &self,
         target: &mut T
     ) -> Result<(), ShortBuf> {
-        if let Some(value) = self.0 {
-            value.compose(target)?;
-        }
-        Ok(())
+       self.0.compose(target)
     }
 }
 
