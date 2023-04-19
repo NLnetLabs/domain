8d7fe2b1aa2a81ff35f11e817f98bbfcdfa112d3
Philip Homburg <philip@nlnetlabs.nl>
2023-04-19 10:46:12 +0200

    Added example.

--- src/net/client/octet_stream.rs
+++ src/net/client/octet_stream.rs
@@ -1,4 +1,44 @@
 //! A DNS over octet stream transport
+//! # Example
+//! ```
+//! use domain::base::Dname;
+//! use domain::base::Rtype::Aaaa;
+//! use domain::base::{MessageBuilder, StaticCompressor, StreamTarget};
+//! use domain::net::client::octet_stream::Connection;
+//! use std::net::{IpAddr, SocketAddr};
+//! use std::str::FromStr;
+//! use tokio::net::TcpStream;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     // Create DNS request message
+//!     // Create a message builder wrapping a compressor wrapping a stream
+//!     // target.
+//!     let mut msg =
+//!         MessageBuilder::from_target(StaticCompressor::new(StreamTarget::new_vec())).unwrap();
+//!     msg.header_mut().set_rd(true);
+//!     let mut msg = msg.question();
+//!     msg.push((Dname::<Vec<u8>>::vec_from_str("example.com").unwrap(), Aaaa))
+//!         .unwrap();
+//!     let mut msg = msg.as_builder_mut().clone();
+//!
+//!     let server_addr = SocketAddr::new(IpAddr::from_str("::1").unwrap(), 53);
+//!
+//!     let tcp = TcpStream::connect(server_addr).await.unwrap();
+//!     let (reader, writer) = tcp.into_split();
+//!
+//!     let conn = Connection::new().unwrap();
+//!     let conn_run = conn.clone();
+//!
+//!     tokio::spawn(async move {
+//!         conn_run.run(reader, writer).await;
+//!     });
+//!
+//!     let mut query = conn.query(&mut msg).await.unwrap();
+//!     let reply = query.get_result().await;
+//!     println!("reply: {:?}", reply);
+//! }
+//! ```
 
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
