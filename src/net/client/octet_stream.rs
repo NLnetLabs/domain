eec1f85023b16d9851841f0b51e6802de1af1928
Philip Homburg <philip@nlnetlabs.nl>
2023-07-20 15:29:49 +0200

    Ignore existing TcpKeepalive

--- src/net/client/octet_stream.rs
+++ src/net/client/octet_stream.rs
@@ -1025,7 +1025,7 @@ fn add_tcp_keepalive<Octs: Clone + Composer + OctetsBuilder>(
                     {
                         let option = option.unwrap();
                         if let AllOptData::TcpKeepalive(_) = option {
-                            panic!("handle keepalive");
+                            // Ignore existing TcpKeepalive
                         } else {
                             newopt.push(&option).unwrap();
                         }
