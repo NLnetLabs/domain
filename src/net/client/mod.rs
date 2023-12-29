bacdca9064d698ae848ec2504c4431509c620320
Philip Homburg <philip@nlnetlabs.nl>
2023-12-29 14:40:43 +0100

    More documentation for net::client.

--- src/net/client/mod.rs
+++ src/net/client/mod.rs
@@ -1,4 +1,102 @@
 //! Sending requests and receiving responses.
+//!
+//! This module provides DNS transport protocols that allow sending a DNS
+//! request and receiving the corresponding reply.
+//!
+//! Sending a request and receiving the reply consists of four steps:
+//! 1) Creating a request message,
+//! 2) Creating a DNS transport,
+//! 3) Sending the request, and
+//! 4) Receiving the reply.
+//!
+//! The first and second step are independent and happen in any order.
+//! The third step uses the resuts of the first and second step.
+//! Finally, the fourth step uses the result of the third step.
+
+//! # Creating a request message
+//!
+//! The DNS transport protocols expect a request message that implements
+//! [ComposeRequest][request::ComposeRequest] trait.
+//! This trait allows transports to add ENDS(0) options, set flags, etc.
+//! The [RequestMessage][request::RequestMessage] type implements this trait.
+//! The [new][request::RequestMessage::new] method of RequestMessage create
+//! a new RequestMessage object based an existing messsage (that implements
+//! ```Into<Message<Octs>>```).
+//!
+//! For example:
+//! ```rust
+//! let mut msg = MessageBuilder::new_vec();
+//! msg.header_mut().set_rd(true);
+//! let mut msg = msg.question();
+//! msg.push((Dname::vec_from_str("example.com").unwrap(), Aaaa)).unwrap();
+//! let req = RequestMessage::new(msg);
+//! ```
+
+//! # Creating a DNS transport
+//!
+//! Creating a DNS transport typically involves creating a configuration
+//! object, creating the underlying network connection, creating the
+//! DNS transport and running a ```run``` method as a separate task. This
+//! is illustrated in the following example:
+//! ```rust
+//! let multi_stream_config = multi_stream::Config {
+//!        stream: Some(stream::Config {
+//!            response_timeout: Duration::from_millis(100),
+//!        }),
+//!    };
+//! let tcp_connect = TcpConnect::new(server_addr);
+//! let tcp_conn =
+//!        multi_stream::Connection::new(Some(multi_stream_config.clone()))
+//!            .unwrap();
+//! let run_fut = tcp_conn.run(tcp_connect);
+//! tokio::spawn(async move {
+//!        let res = run_fut.await;
+//! });
+//! ```
+//! Note that the run function ends when the last reference to the DNS
+//! transport is dropped. For this reason it is important to avoid having a
+//! reference to the transport end up in the task. Only pass the future
+//! returned by the run function to the task.
+//!
+//! The currently implemented DNS transport have the following layering. At
+//! the lower layer are [dgram] and [stream]. The dgram transport is used for
+//! DNS over UDP, the stream transport is used for DNS over a single TCP or
+//! TLS connection. The transport works as long as the connection continuous
+//! to exist.
+//! The [multi_stream] transport is layered on top of stream, and creates new
+//! TCP or TLS connections when old ones terminates.
+//! Next, [dgram_stream] combines the dgram transport with the multi_stream
+//! transport. This is typically needed because a request over UDP can receive
+//! a truncated response, which should be retried over TCP.
+//! Finally, the [redundant] transport can select the best transport out of
+//! a collection of underlying transports.
+
+//! # Sending the request
+//!
+//! A DNS transport implements the [SendRequest][request::SendRequest] trait.
+//! This trait provides a single method,
+//! [send_request][request::SendRequest::send_request] and returns an object
+//! that provides the response.
+//!
+//! For example:
+//! ```rust
+//! let mut request = tls_conn.send_request(&req).await.unwrap();
+//! ```
+//! where ```tls_conn``` is a transport connection for DNS over TLS.
+
+//! # Receiving the request
+//!
+//! The [send_request][request::SendRequest::send_request] method returns an
+//! object that implements the [GetResponse][request::GetResponse] trait.
+//! This trait provides a single method,
+//! [get_response][request::GetResponse::get_response], which returns the
+//! DNS response message or an error. This method is intended to be
+//! cancelation safe.
+//!
+//! For example:
+//! ```rust
+//! let reply = request.get_response().await;
+//! ```
 
 //! # Example with various transport connections
 //! ```
