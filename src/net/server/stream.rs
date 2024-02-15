7dcfc8fb5efd7a138be5e2b066bac96bfcaa9a95
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-02-15 15:55:58 +0100

    cargo fmt.

--- src/net/server/stream.rs
+++ src/net/server/stream.rs
@@ -257,7 +257,7 @@ where
     /// # Drop behaviour
     ///
     /// When dropped [`shutdown()`] will be invoked.
-    /// 
+    ///
     /// [`shutdown()`]: Self::shutdown
     pub async fn run(&self)
     where
