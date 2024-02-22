08e5cb250dd188b4bbd704fad5bb144135b62db9
Martin Hoffmann <martin@nlnetlabs.nl>
2024-02-22 13:41:15 +0100

    Fix SvcParamKey mnemonics.

--- src/base/iana/svcb.rs
+++ src/base/iana/svcb.rs
@@ -4,16 +4,16 @@ int_enum! {
     =>
     SvcParamKey, u16;
 
-    (MANDATORY => 0, b"Mandatory keys in this RR")
-    (ALPN => 1, b"Additional supported protocols")
-    (NO_DEFAULT_ALPN => 2, b"Additional supported protocols")
-    (PORT => 3, b"Port for alternative endpoint")
-    (IPV4HINT => 4, b"IPv4 address hints")
+    (MANDATORY => 0, b"mandatory")
+    (ALPN => 1, b"alpn")
+    (NO_DEFAULT_ALPN => 2, b"no-default-alpn")
+    (PORT => 3, b"port")
+    (IPV4HINT => 4, b"ipv4hint")
     // https://datatracker.ietf.org/doc/draft-ietf-tls-esni/
-    (ECH => 5, b"Encrypted ClientHello info")
-    (IPV6HINT => 6, b"IPv6 address hints")
+    (ECH => 5, b"ech")
+    (IPV6HINT => 6, b"ipv6hint")
     // https://datatracker.ietf.org/doc/draft-ietf-add-svcb-dns/
-    (DOHPATH => 7, b"DNS over HTTPS path template")
+    (DOHPATH => 7, b"dohpath")
 }
 
 int_enum_str_with_prefix!(SvcParamKey, "key", b"key", u16, "unknown key");
