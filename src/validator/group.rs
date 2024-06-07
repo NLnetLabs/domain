f72f57d1256779018c6d88648a0858bf0d232c7d
Philip Homburg <philip@nlnetlabs.nl>
2024-06-07 17:34:52 +0200

    Fmt

--- src/validator/group.rs
+++ src/validator/group.rs
@@ -118,8 +118,7 @@ impl Group {
             if frr.owner() != &rr.owner().to_name::<Bytes>() {
                 return Err(());
             }
-	} else if *self.sig_set[0].owner() != rr.owner()
-        {
+        } else if *self.sig_set[0].owner() != rr.owner() {
             return Err(());
         }
 
