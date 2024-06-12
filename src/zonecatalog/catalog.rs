834e5782d7c2fdf65d0d9a4969fe9528553bb032
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-06-12 12:25:49 +0200

    RustDoc and feature gate fixes.

--- src/zonecatalog/catalog.rs
+++ src/zonecatalog/catalog.rs
@@ -1,3 +1,4 @@
+//! Experimental storing, querying and syncing a catalog of zones.
 // Note to self: This design ties XFR and catalogs together, but really they
 // are separate things. Maybe better have triggers/callbacks or something for
 // joining separate pieces together, including the XFR-in XfrMiddlewareService
--- src/zonecatalog/mod.rs
+++ src/zonecatalog/mod.rs
@@ -1,1 +1,15 @@
+#![cfg(all(
+    feature = "unstable-zonetree",
+    feature = "unstable-client-transport"
+))]
+#![cfg_attr(
+    docsrs,
+    doc(cfg(all(
+        feature = "unstable-zonetree",
+        feature = "unstable-client-transport"
+    )))
+)]
+#![warn(missing_docs)]
+//! Experimental storing, querying and syncing a catalog of zones.
+
 pub mod catalog;
