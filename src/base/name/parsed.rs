22c4d20090b3700da10a4e2a0793f38435837dac
Terts Diepraam <terts.diepraam@gmail.com>
2024-06-21 16:08:20 +0200

    add serde::Serialize for Opt records

--- src/base/name/parsed.rs
+++ src/base/name/parsed.rs
@@ -514,7 +514,18 @@ impl<Octs: AsRef<[u8]>> fmt::Display for ParsedName<Octs> {
 
 impl<Octs: AsRef<[u8]>> fmt::Debug for ParsedName<Octs> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "ParsedName({}.)", self)
+        write!(f, "ParsedName({}.)", self)    }
+}
+
+//--- Serialize
+
+#[cfg(feature = "serde")]
+impl<Octs: AsRef<[u8]>> serde::Serialize for ParsedName<Octs> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer {
+        use std::string::ToString;
+        self.to_string().serialize(serializer)
     }
 }
 
