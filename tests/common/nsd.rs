c8a2dfc93b7094b970d05a4a0175459bfb60320b
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-07-24 21:27:48 +0200

    Cleanup imports.

--- tests/common/nsd.rs
+++ tests/common/nsd.rs
@@ -1,13 +1,14 @@
 //! Configuring and running NSD.
 
-use bytes::Bytes;
-use domain::utils::base64;
 use std::fs::File;
 use std::net::{IpAddr, SocketAddr};
 use std::path::{Path, PathBuf};
 use std::string::String;
 use std::vec::Vec;
 use std::{fmt, io};
+
+use bytes::Bytes;
+use domain::utils::base64;
 
 //------------ Config --------------------------------------------------------
 
