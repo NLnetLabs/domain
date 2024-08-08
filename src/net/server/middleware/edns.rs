6e7a7540b08ec59b1814555d2d4b17ae4ff3d900
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-08-08 10:47:48 +0200

    Remove EDNS fixes that are also dealt with by separate work in branch fix-edns-middleware / PR #355.

--- src/net/server/middleware/edns.rs
+++ src/net/server/middleware/edns.rs
@@ -207,7 +207,7 @@ where
                             ));
                         }
 
-                        TransportSpecificContext::NonUdp(ctx) => {
+                        TransportSpecificContext::NonUdp(_) => {
                             // https://datatracker.ietf.org/doc/html/rfc7828#section-3.2.1
                             // 3.2.1. Sending Queries
                             //   "Clients MUST specify an OPTION-LENGTH of 0
@@ -222,16 +222,6 @@ where
                                             request.message(),
                                             OptRcode::FORMERR,
                                         ),
-                                    );
-                                }
-                            }
-
-                            if let Some(keep_alive) = ctx.idle_timeout() {
-                                if let Ok(timeout) =
-                                    IdleTimeout::try_from(keep_alive)
-                                {
-                                    Self::reserve_space_for_keep_alive_opt(
-                                        request, timeout,
                                     );
                                 }
                             }
@@ -303,12 +293,6 @@ where
                                 // timeout is known: "Signal the timeout value
                                 // using the edns-tcp-keepalive EDNS(0) option
                                 // [RFC7828]".
-
-                                // Remove the limit we should have imposed
-                                // during pre-processing so that we can use
-                                // the space we reserved for the OPT RR.
-                                response.clear_push_limit();
-
                                 if let Err(err) =
                                     // TODO: Don't add the option if it
                                     // already exists?
@@ -337,34 +321,6 @@ where
         // TODO: For UDP EDNS capable clients (those that included an OPT
         // record in the request) should we set the Requestor's Payload Size
         // field to some value?
-    }
-
-    fn reserve_space_for_keep_alive_opt(
-        request: &mut Request<RequestOctets, RequestMeta>,
-        timeout: IdleTimeout,
-    ) {
-        // TODO: Calculate this once as a const value, not on every request.
-
-        let keep_alive_opt = TcpKeepalive::new(Some(timeout));
-        let root_name_len = Name::root_ref().compose_len();
-
-        // See:
-        //  - https://datatracker.ietf.org/doc/html/rfc1035#section-3.2.1
-        //  - https://datatracker.ietf.org/doc/html/rfc6891#autoid-12
-        //  - https://datatracker.ietf.org/doc/html/rfc7828#section-3.1
-
-        // Calculate the size of the DNS OPTION RR that will be added to the
-        // response during post-processing.
-        let wire_opt_len = root_name_len // "0" root domain name per RFC 6891
-            + 2 // TYPE
-            + 2 // CLASS
-            + 4 // TTL
-            + 2 // RDLEN
-            + 2 // OPTION-CODE
-            + 2 // OPTION-LENGTH
-            + keep_alive_opt.compose_len(); // OPTION-DATA
-
-        request.reserve_bytes(wire_opt_len);
     }
 
     fn map_stream_item(
