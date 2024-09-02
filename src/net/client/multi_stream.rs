88d7087ef2489171e7db36d9a0d6d9529a779143
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-09-02 23:12:35 +0200

    Remove outdated comment.

--- src/net/client/multi_stream.rs
+++ src/net/client/multi_stream.rs
@@ -356,9 +356,6 @@ impl<Req: ComposeRequest + Clone + 'static> GetResponse for Request<Req> {
     > {
         Box::pin(Self::get_response(self))
     }
-
-    // TODO: Override stream_complete() and is_stream_complete() like
-    // net::client::stream does?
 }
 
 //------------ Transport ------------------------------------------------
