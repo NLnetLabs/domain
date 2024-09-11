12f1ebfea04cfe64e112949945fbe5fa3559cb57
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-09-11 17:27:30 +0200

    Typo correction.

--- src/net/client/tsig.rs
+++ src/net/client/tsig.rs
@@ -41,7 +41,7 @@
 //!
 //! Failing to do so will result in signature verification failure. For
 //! requests this will occur at the receiving server. For responses this will
-//! result in [`GetResponse`] rerturning
+//! result in [`GetResponse`] returning
 //! [Error::ValidationError][crate::net::client::request::Error].
 #![cfg(all(feature = "tsig", feature = "unstable-client-transport"))]
 #![warn(missing_docs)]
