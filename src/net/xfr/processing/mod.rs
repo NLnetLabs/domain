b1c75bc568c4116a0e6491487b7152eee22fec20
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-09-16 11:02:19 +0200

    Minor RustDoc language improvement.

--- src/net/xfr/processing/mod.rs
+++ src/net/xfr/processing/mod.rs
@@ -1,6 +1,6 @@
 //! Parsing of AXFR/IXFR response messages for higher level processing.
 //!
-//! This module provides [`XfrResponseProcessor`] which enables you to process
+//! This module provides [`XfrResponseProcessor`] which can be used to process
 //! one or more AXFR/IXFR response messages in terms of the high level
 //! [`XfrEvent`]s that they represent without having to deal with the
 //! AXFR/IXFR protocol details.
