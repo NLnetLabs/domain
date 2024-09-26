863636c2bab18c9dc6f7cea553b6f90b405ffd42
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-09-26 12:23:07 +0200

    RustDoc fixes.

--- src/net/server/middleware/xfr/mod.rs
+++ src/net/server/middleware/xfr/mod.rs
@@ -10,14 +10,10 @@
 //! provided allowing those types to be used as-is as XFR data providers with
 //! this middleware service.
 //!
-//! [`XfrRrBatcher`], primarily intended for internal use by
-//! [`XfrMiddlewareSvc`], handles splitting of large zone transfer replies
-//! into batches with as many resource records per response as will fit.
-//!
 //! # Requiring TSIG authenticated XFR requests
 //!
 //! To require XFR requests to be TSIG authenticated, implement
-//! [`XfrDataProvider<Option<Key>>`], extract the key data using
+//! `XfrDataProvider<Option<Key>>`, extract the key data using
 //! [`Request::metadata()`] and verify that a TSIG key was used to sign the
 //! request, and that the name and algorithm of the used key are acceptable to
 //! you.
@@ -35,8 +31,12 @@
 //!
 //! [RFC 5936]: https://www.rfc-editor.org/info/rfc5936
 //! [RFC 1995]: https://www.rfc-editor.org/info/rfc1995
+//! [`Request::metadata()`]: crate::net::server::message::Request::metadata
 //! [`TsigMiddlewareSvc`]:
 //!     crate::net::server::middleware::tsig::TsigMiddlewareSvc
+//! [`XfrDataProvider`]: super::data_provider::XfrDataProvider
+//! [`Zone`]: crate::net::zonetree::Zone
+//! [`ZoneTree`]: crate::net::zonetree::ZoneTree
 mod axfr;
 mod batcher;
 mod ixfr;
