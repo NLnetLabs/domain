030c68e747756c8beec9a1cb7b2f09b518c86afd
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-09-27 13:50:45 +0200

    Fix RustDoc broken links and clarify limitation note.

--- src/zonetree/in_memory/mod.rs
+++ src/zonetree/in_memory/mod.rs
@@ -3,11 +3,11 @@
 //! # Limitations
 //!
 //! * There is currently no support for removing old versions of zone data
-//!   stored in the tree. The only options are to [`rollback()`] the newest
-//!   version or [`walk()`] the [`Zone`] cloning the current version into a
-//!   new [`Zone`] then dropping the old [`Zone`].
-//! 
-//! [`Zone`]: super::Zone
+//!   stored in the tree. The only optionis to [`walk()`] the [`Zone`] cloning
+//!   the current version into a new [`Zone`] then dropping the old [`Zone`].
+//!
+//! [`Zone`]: crate::zonetree::Zone
+//! [`walk()`]: crate::zoneree::ReadableZone::walk()
 mod builder;
 mod nodes;
 mod read;
