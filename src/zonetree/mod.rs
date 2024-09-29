a112b1e151d115a5875d7a1fb1a841ae2861b807
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-09-29 12:03:44 +0200

    Expose ZoneKey.

--- src/zonetree/mod.rs
+++ src/zonetree/mod.rs
@@ -124,7 +124,7 @@ pub use self::types::{
     StoredName, StoredRecord,
 };
 pub use self::walk::WalkOp;
-pub use self::zone::Zone;
+pub use self::zone::{Zone, ZoneKey};
 
 /// Zone related utilities.
 pub mod util {
