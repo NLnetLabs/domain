ac8bf7589e27e1aa9f68392d8a81f76188cf5a9f
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-10-03 13:34:55 +0200

    FIX: Servers should drop received DNS response messages, not propagate them for processing. (#381)

--- src/net/server/dgram.rs
+++ src/net/server/dgram.rs
@@ -511,7 +511,18 @@ where
                     tokio::spawn(async move {
                         match Message::from_octets(buf) {
                             Err(err) => {
-                                tracing::warn!("Failed while parsing request message: {err}");
+                                // TO DO: Count this event?
+                                warn!("Failed while parsing request message: {err}");
+                            }
+
+                            // https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
+                            // 4.1.1. Header section format
+                            //   "QR   A one bit field that specifies whether
+                            //         this message is a query (0), or a
+                            //         response (1)."
+                            Ok(msg) if msg.header().qr() => {
+                                // TO DO: Count this event?
+                                trace!("Ignoring received message because it is a reply, not a query.");
                             }
 
                             Ok(msg) => {
