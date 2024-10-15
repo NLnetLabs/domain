94541da08ba59e0949e7ab1411961ce50e8a798d
arya dradjica <arya@nlnetlabs.nl>
2024-10-15 17:32:36 +0200

    Refactor the 'sign' module
    Most functions have been renamed.  The public key types have been moved
    to the 'validate' module (which 'sign' now depends on), and they have
    been outfitted with conversions (e.g. to and from DNSKEY records).
    
    Importing a generic key into an OpenSSL or Ring key now requires the
    public key to also be available.  In both implementations, the pair are
    checked for consistency -- this ensures that both are uncorrupted and
    that keys have not been mixed up.  This also allows the Ring backend to
    support ECDSA keys (although key generation is still difficult).
    
    The 'PublicKey' and 'PrivateKey' enums now store their array data in
    'Box'.  This has two benefits: it is easier to securely manage memory
    on the heap (since the compiler will not copy it around the stack); and
    the smaller sizes of the types is beneficial (although negligibly) to
    performance.

--- src/sign/mod.rs
+++ src/sign/mod.rs
@@ -2,37 +2,44 @@
 //!
 //! **This module is experimental and likely to change significantly.**
 //!
-//! Signatures are at the heart of DNSSEC -- they confirm the authenticity of a
-//! DNS record served by a secure-aware name server.  But name servers are not
-//! usually creating those signatures themselves.  Within a DNS zone, it is the
-//! zone administrator's responsibility to sign zone records (when the record's
-//! time-to-live expires and/or when it changes).  Those signatures are stored
-//! as regular DNS data and automatically served by name servers.
+//! Signatures are at the heart of DNSSEC -- they confirm the authenticity of
+//! a DNS record served by a security-aware name server.  Signatures can be
+//! made "online" (in an authoritative name server while it is running) or
+//! "offline" (outside of a name server).  Once generated, signatures can be
+//! serialized as DNS records and stored alongside the authenticated records.
 
 #![cfg(feature = "sign")]
 #![cfg_attr(docsrs, doc(cfg(feature = "sign")))]
 
-use crate::base::iana::SecAlg;
+use crate::{
+    base::iana::SecAlg,
+    validate::{PublicKey, Signature},
+};
 
 pub mod generic;
-pub mod key;
 pub mod openssl;
-pub mod records;
 pub mod ring;
 
-/// Signing DNS records.
+/// Sign DNS records.
 ///
-/// Implementors of this trait own a private key and sign DNS records for a zone
-/// with that key.  Signing is a synchronous operation performed on the current
-/// thread; this rules out implementations like HSMs, where I/O communication is
-/// necessary.
-pub trait Sign<Buffer> {
-    /// An error in constructing a signature.
-    type Error;
-
+/// Types that implement this trait own a private key and can sign arbitrary
+/// information (for zone signing keys, DNS records; for key signing keys,
+/// subsidiary public keys).
+///
+/// Before a key can be used for signing, it should be validated.  If the
+/// implementing type allows [`sign()`] to be called on unvalidated keys, it
+/// will have to check the validity of the key for every signature; this is
+/// unnecessary overhead when many signatures have to be generated.
+///
+/// [`sign()`]: Sign::sign()
+pub trait Sign {
     /// The signature algorithm used.
     ///
-    /// The following algorithms can be used:
+    /// The following algorithms are known to this crate.  Recommendations
+    /// toward or against usage are based on published RFCs, not the crate
+    /// authors' opinion.  Implementing types may choose to support some of
+    /// the prohibited algorithms anyway.
+    ///
     /// - [`SecAlg::RSAMD5`] (highly insecure, do not use)
     /// - [`SecAlg::DSA`] (highly insecure, do not use)
     /// - [`SecAlg::RSASHA1`] (insecure, not recommended)
@@ -47,11 +54,35 @@ pub trait Sign<Buffer> {
     /// - [`SecAlg::ED448`]
     fn algorithm(&self) -> SecAlg;
 
-    /// Compute a signature.
+    /// The public key.
     ///
-    /// A regular signature of the given byte sequence is computed and is turned
-    /// into the selected buffer type.  This provides a lot of flexibility in
-    /// how buffers are constructed; they may be heap-allocated or have a static
-    /// size.
-    fn sign(&self, data: &[u8]) -> Result<Buffer, Self::Error>;
+    /// This can be used to verify produced signatures.  It must use the same
+    /// algorithm as returned by [`algorithm()`].
+    ///
+    /// [`algorithm()`]: Self::algorithm()
+    fn public_key(&self) -> PublicKey;
+
+    /// Sign the given bytes.
+    ///
+    /// # Errors
+    ///
+    /// There are three expected failure cases for this function:
+    ///
+    /// - The secret key was invalid.  The implementing type is responsible
+    ///   for validating the secret key during initialization, so that this
+    ///   kind of error does not occur.
+    ///
+    /// - Not enough randomness could be obtained.  This applies to signature
+    ///   algorithms which use randomization (primarily ECDSA).  On common
+    ///   platforms like Linux, Mac OS, and Windows, cryptographically secure
+    ///   pseudo-random number generation is provided by the OS, so this is
+    ///   highly unlikely.
+    ///
+    /// - Not enough memory could be obtained.  Signature generation does not
+    ///   require significant memory and an out-of-memory condition means that
+    ///   the application will probably panic soon.
+    ///
+    /// None of these are considered likely or recoverable, so panicking is
+    /// the simplest and most ergonomic solution.
+    fn sign(&self, data: &[u8]) -> Signature;
 }
