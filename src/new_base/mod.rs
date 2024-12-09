ea600fd3f5e666c2812a29c15011f77190b76042
arya dradjica <arya@nlnetlabs.nl>
2024-12-09 17:36:41 +0100

    [new_base] Add module 'name'

--- src/new_base/mod.rs
+++ src/new_base/mod.rs
@@ -3,3 +3,5 @@
 //! This module provides the essential types and functionality for working
 //! with DNS.  Most importantly, it provides functionality for parsing and
 //! building DNS messages on the wire.
+
+pub mod name;
---
+++ src/new_base/name/mod.rs
@@ -0,0 +1,15 @@
+//! Domain names.
+//!
+//! Domain names are a core concept of DNS.  The whole system is essentially
+//! just a mapping from domain names to arbitrary information.  This module
+//! provides types and essential functionality for working with them.
+//!
+//! A domain name is a sequence of labels, separated by ASCII periods (`.`).
+//! For example, `example.org.` contains three labels: `example`, `org`, and
+//! `` (the root label).  Outside DNS-specific code, the root label (and its
+//! separator) are almost always omitted, but keep them in mind here.
+//!
+//! Domain names form a hierarchy, where `b.a` is the "parent" of `.c.b.a`.
+//! The owner of `example.org` is thus responsible for _every_ domain ending
+//! with the `.example.org` suffix.  The reverse order in which this hierarchy
+//! is expressed can sometimes be confusing.
