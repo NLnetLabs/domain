NLnetLabs/domain

commit 51fc72b2178765e8ac446f1fa7d78a266478bc7e

Handle quoted octets. (#470)

src/zonefile/inplace.rs
@@ -564,11 +564,21 @@ impl Scanner for EntryScanner<'_> {
         // trim off everything to the left already.
         self.zonefile.buf.trim_to(self.zonefile.buf.start);
 
+        // Remember if we are inside a quoted value. If so the opening quote
+        // has already been skipped over, it is not part of the value.
+        let is_quoted = self.zonefile.buf.cat == ItemCat::Quoted;
+
         // Skip over symbols that don’t need converting at the beginning.
         while self.zonefile.buf.next_ascii_symbol()?.is_some() {}
 
         // If we aren’t done yet, we have escaped characters to replace.
         let mut write = self.zonefile.buf.start;
+
+        if is_quoted {
+            // Discard the closing quote, it shouldn't be part of the value.
+            write -= 1;
+        }
+
         while let Some(sym) = self.zonefile.buf.next_symbol()? {
             self.zonefile.buf.buf[write] = sym.into_octet()?;
             write += 1;
@@ -1384,7 +1394,7 @@ impl SourceBuf {
 //------------ ItemCat -------------------------------------------------------
 
 /// The category of the current item in a source buffer.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum ItemCat {
     /// We don’t currently have an item.
     ///
@@ -1531,6 +1541,26 @@ mod test {
         let entry = EntryScanner::new(&mut zone).unwrap();
         entry.zonefile.buf.next_item().unwrap();
         op(entry)
+    }
+
+    #[test]
+    fn scan_octets() {
+        #[track_caller]
+        fn test(zone: &str, tok: impl AsRef<[u8]>) {
+            with_entry(zone, |mut entry| {
+                let res = entry.scan_octets().unwrap();
+                assert_eq!(&res[..], tok.as_ref());
+            });
+        }
+
+        test(" unquoted\r\n", b"unquoted");
+        test(" unquoted  ", b"unquoted");
+        test("unquoted ", b"unquoted");
+        test("unqu\\oted ", b"unquoted");
+        test("unqu\\111ted ", b"unquoted");
+        test(" \"quoted\"\n", b"quoted");
+        test(" \"quoted\" ", b"quoted");
+        test("\"quoted\" ", b"quoted");
     }
 
     #[test]

commit 3011d4c1a74981e7a790ea8cb658144c8fd5c50c

[zonefile/inplace] Handle backslashes in quoted strings (#472)
This PR fixes an issue when parsing the first backslash of a quoted
string introduced by #470.

src/zonefile/inplace.rs
@@ -571,13 +571,19 @@ impl Scanner for EntryScanner<'_> {
         // Skip over symbols that don’t need converting at the beginning.
         while self.zonefile.buf.next_ascii_symbol()?.is_some() {}
 
+        if self.zonefile.buf.cat == ItemCat::None {
+            // The item has ended.  Remove the double quote.
+            let write = if is_quoted {
+                self.zonefile.buf.start - 1
+            } else {
+                self.zonefile.buf.start
+            };
+            self.zonefile.buf.next_item()?;
+            return Ok(self.zonefile.buf.split_to(write).freeze());
+        }
+
         // If we aren’t done yet, we have escaped characters to replace.
         let mut write = self.zonefile.buf.start;
-
-        if is_quoted {
-            // Discard the closing quote, it shouldn't be part of the value.
-            write -= 1;
-        }
 
         while let Some(sym) = self.zonefile.buf.next_symbol()? {
             self.zonefile.buf.buf[write] = sym.into_octet()?;
@@ -1561,6 +1567,8 @@ mod test {
         test(" \"quoted\"\n", b"quoted");
         test(" \"quoted\" ", b"quoted");
         test("\"quoted\" ", b"quoted");
+        test("\"qu\\oted\"", b"quoted");
+        test(" \"qu\\\\ot\\\\ed\" ", b"qu\\ot\\ed");
     }
 
     #[test]