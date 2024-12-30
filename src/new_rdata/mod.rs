1133b4c0860cef74a095dfa10caaaa6a1e49fcb7
arya dradjica <arya@nlnetlabs.nl>
2024-12-30 14:08:55 +0100

    [new_rdata] Inline 'rfc1035' and support 'rfc3596'

--- src/new_rdata/mod.rs
+++ src/new_rdata/mod.rs
@@ -1,3 +1,7 @@
 //! Record data types.
 
-pub mod rfc1035;
+mod rfc1035;
+pub use rfc1035::{Cname, Hinfo, Mx, Ns, Ptr, Soa, Txt, Wks, A};
+
+mod rfc3596;
+pub use rfc3596::Aaaa;
---
+++ src/new_rdata/rfc3596.rs
@@ -0,0 +1,98 @@
+//! IPv6 record data types.
+
+#[cfg(feature = "std")]
+use core::{fmt, str::FromStr};
+
+#[cfg(feature = "std")]
+use std::net::Ipv6Addr;
+
+use zerocopy::IntoBytes;
+use zerocopy_derive::*;
+
+use crate::new_base::build::{
+    self, BuildInto, BuildIntoMessage, TruncationError,
+};
+
+//----------- Aaaa -----------------------------------------------------------
+
+/// The IPv6 address of a host responsible for this domain.
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    FromBytes,
+    IntoBytes,
+    KnownLayout,
+    Immutable,
+    Unaligned,
+)]
+#[repr(transparent)]
+pub struct Aaaa {
+    /// The IPv6 address octets.
+    pub octets: [u8; 16],
+}
+
+//--- Converting to and from 'Ipv6Addr'
+
+#[cfg(feature = "std")]
+impl From<Ipv6Addr> for Aaaa {
+    fn from(value: Ipv6Addr) -> Self {
+        Self {
+            octets: value.octets(),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<Aaaa> for Ipv6Addr {
+    fn from(value: Aaaa) -> Self {
+        Self::from(value.octets)
+    }
+}
+
+//--- Parsing from a string
+
+#[cfg(feature = "std")]
+impl FromStr for Aaaa {
+    type Err = <Ipv6Addr as FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ipv6Addr::from_str(s).map(Aaaa::from)
+    }
+}
+
+//--- Formatting
+
+#[cfg(feature = "std")]
+impl fmt::Display for Aaaa {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        Ipv6Addr::from(*self).fmt(f)
+    }
+}
+
+//--- Building into DNS messages
+
+impl BuildIntoMessage for Aaaa {
+    fn build_into_message(
+        &self,
+        builder: build::Builder<'_>,
+    ) -> Result<(), TruncationError> {
+        self.as_bytes().build_into_message(builder)
+    }
+}
+
+//--- Building into byte strings
+
+impl BuildInto for Aaaa {
+    fn build_into<'b>(
+        &self,
+        bytes: &'b mut [u8],
+    ) -> Result<&'b mut [u8], TruncationError> {
+        self.as_bytes().build_into(bytes)
+    }
+}
