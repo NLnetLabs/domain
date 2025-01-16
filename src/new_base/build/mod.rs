477252047ef16a64c25c1f5426d02db863eae53f
arya dradjica <arya@nlnetlabs.nl>
2025-01-16 12:47:08 +0100

    [new_base/build] Document 'BuildCommitted' thoroughly

--- src/new_base/build/mod.rs
+++ src/new_base/build/mod.rs
@@ -38,13 +38,63 @@ impl BuildIntoMessage for [u8] {
 //----------- BuildResult ----------------------------------------------------
 
 /// The result of building into a DNS message.
+///
+/// This is used in [`BuildIntoMessage::build_into_message()`].
 pub type BuildResult = Result<BuildCommitted, TruncationError>;
 
 //----------- BuildCommitted -------------------------------------------------
 
 /// The output of [`Builder::commit()`].
 ///
-/// This is a stub type to remind users to call [`Builder::commit()`] in all
-/// success paths of building functions.
+/// This is a simple marker type, produced by [`Builder::commit()`].  Certain
+/// trait methods (e.g. [`BuildIntoMessage::build_into_message()`]) require it
+/// in the return type, as a way to remind users to commit their builders.
+///
+/// # Examples
+///
+/// If `build_into_message()` simply returned a unit type, an example impl may
+/// look like:
+///
+/// ```compile_fail
+/// # use domain::new_base::name::RevName;
+/// # use domain::new_base::build::{BuildIntoMessage, Builder, BuildResult};
+/// # use domain::new_base::wire::AsBytes;
+///
+/// struct Foo<'a>(&'a RevName, u8);
+///
+/// impl BuildIntoMessage for Foo<'_> {
+///     fn build_into_message(
+///         &self,
+///         mut builder: Builder<'_>,
+///     ) -> BuildResult {
+///         builder.append_name(self.0)?;
+///         builder.append_bytes(self.1.as_bytes());
+///         Ok(())
+///     }
+/// }
+/// ```
+///
+/// This code is incorrect: since the appended content is not committed, the
+/// builder will remove it when it is dropped (at the end of the function),
+/// and so nothing gets written.  Instead, users have to write:
+///
+/// ```
+/// # use domain::new_base::name::RevName;
+/// # use domain::new_base::build::{BuildIntoMessage, Builder, BuildResult};
+/// # use domain::new_base::wire::AsBytes;
+///
+/// struct Foo<'a>(&'a RevName, u8);
+///
+/// impl BuildIntoMessage for Foo<'_> {
+///     fn build_into_message(
+///         &self,
+///         mut builder: Builder<'_>,
+///     ) -> BuildResult {
+///         builder.append_name(self.0)?;
+///         builder.append_bytes(self.1.as_bytes());
+///         Ok(builder.commit())
+///     }
+/// }
+/// ```
 #[derive(Debug)]
 pub struct BuildCommitted;
