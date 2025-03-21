634ee8cf4fdcb1cd8c5a17a1762a5f1209896bf7
Terts Diepraam <terts.diepraam@gmail.com>
2025-03-21 11:06:19 +0100

    bit more work on TCP stream

--- examples/new_client.rs
+++ examples/new_client.rs
@@ -3,18 +3,42 @@ use std::net::SocketAddr;
 use domain::new_base::name::RevName;
 use domain::new_base::{QClass, QType, Question};
 use domain::new_client::exchange::{Allocator, Exchange, ParsedMessage};
-use domain::new_client::{Client, UdpClient, UdpConfig};
+use domain::new_client::tcp::{TcpClient, TcpConfig};
+use domain::new_client::udp::{UdpClient, UdpConfig};
+use domain::new_client::Client;
+use tokio::join;
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    let example = b"\x00\x03org\x07example";
+    let nlnetlabs = b"\x00\x02nl\x09nlnetlabs";
 
+    let addr: SocketAddr = "1.1.1.1:53".parse().unwrap();
+
+    println!("\n=== UDP ===");
+    let client = UdpClient::new(addr, UdpConfig::default());
+    let res = send_request(example, &client).await;
+    println!("{}", res);
+
+    println!("\n=== TCP ===");
+    let stream = TcpStream::connect(addr).await.unwrap();
+    let client = TcpClient::new(stream, TcpConfig::default());
+    let res = join!(
+        send_request(example, &client),
+        send_request(nlnetlabs, &client),
+    );
+    println!("{}", res.0);
+    println!("{}", res.1);
+}
+
+async fn send_request(name: &[u8], client: &impl Client) -> String {
     let mut bump = bumpalo::Bump::new();
     let alloc = Allocator::new(&mut bump);
 
     let mut request = ParsedMessage::default();
     request.flags.request_recursion(true);
-    let name = b"\x00\x03org\x07example\x03www";
     let name = unsafe { RevName::from_bytes_unchecked(name) };
     request
         .questions
@@ -27,10 +51,8 @@ async fn main() {
         metadata: Vec::new(),
     };
 
-    let addr: SocketAddr = "1.1.1.1:53".parse().unwrap();
-    let client = UdpClient::new(addr, UdpConfig::default());
-
     client.request(&mut exchange).await.unwrap();
 
-    println!("{:#?}", exchange.response);
+    // TODO: Nicer output format
+    format!("{:?}", exchange.response)
 }
--- src/new_client/mod.rs
+++ src/new_client/mod.rs
@@ -34,6 +34,8 @@ pub enum ClientError {
     TooManyRequests,
 
     Bug,
+
+    Broken,
 }
 
 impl From<SocketError> for ClientError {
