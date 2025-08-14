9431088bad52250dc6c292f7b3b8f4213bc85207
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-08-14 11:36:39 +0200

    Resolve Clippy error about deprecated functionality in TSIG constant time comparison operation by using the rust-crypto crate fixed_time_eq() fn.

--- src/tsig/mod.rs
+++ src/tsig/mod.rs
@@ -63,7 +63,7 @@ use std::sync::Arc;
 
 use bytes::{Bytes, BytesMut};
 use octseq::octets::Octets;
-use ring::{constant_time, hkdf::KeyType, hmac, rand};
+use ring::{hkdf::KeyType, hmac, rand};
 
 use crate::base::header::HeaderSection;
 use crate::base::iana::{Class, Rcode, TsigRcode};
@@ -360,8 +360,12 @@ impl Key {
         } else {
             expected.as_ref()
         };
-        constant_time::verify_slices_are_equal(expected, provided)
-            .map_err(|_| ValidationError::BadSig)
+
+        if crypto::util::fixed_time_eq(expected, provided) {
+            Ok(())
+        } else {
+            Err(ValidationError::BadSig)
+        }
     }
 
     /// Completes a message by adding a TSIG record.
