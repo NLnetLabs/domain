d10c79e2ab1feef32d0938c4172725331fc581c1
Martin Hoffmann <martin@nlnetlabs.nl>
2026-07-06 15:32:40 +0200

    Allow deprecated Atomic::fetch_update. (#690)
    This PR allows `Atomic::fetch_update` to be deprecated. It is being renamed
    to `Atomic::try_update` but the MSRV of our crate is too low to use the
    new name.

--- examples/server-transports.rs
+++ examples/server-transports.rs
@@ -263,6 +263,8 @@ fn name_to_ip(
 /// The function signature is slightly simpler to write than when not using
 /// [`service_fn`] and supports passing in meta data without any extra
 /// boilerplate.
+#[allow(deprecated)] // fetch_update will be try_update but our MSRV is too
+// low for that.
 fn query(
     request: Request<Vec<u8>, ()>,
     count: Arc<AtomicU8>,
